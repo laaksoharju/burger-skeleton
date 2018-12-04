1ec6a467e1f2aad350b835e004830990d54872a0
johnlk <jlkuhn@purdue.edu>
2018-12-04 20:36:36 +0100

    fixed some alignment issues

--- src/router.js
+++ src/router.js
@@ -10,56 +10,56 @@ import OrderCompletedPage from './views/OrderCompletedPage.vue';
 Vue.use(Router);
 
 let router = new Router({
-    routes: [
-        {
-            path: '/',
-            meta: {
-                title: 'Welcome to Burger Heaven',
-            },
-            name: 'Start',
-            component: StartPage
-        },
-        {
-            path: '/OrderPage',
-            meta: {
-                title: 'Order Screen',
-            },
-            name: 'OrderPage',
-            component: OrderPage
-        },
-        {
-            path: '/OrderSummary',
-            meta: {
-                title: 'Order Summary'
-            },
-            name: 'Summary',
-            component: OrderSummaryPage
-        },
-        {
-            path: '/OrderCompleted',
-            meta: {
-                title: 'Order Completed',
-            },
-            name: 'Completed',
-            component: OrderCompletedPage
-        },
-        {
-            path: '/kitchen',
-            meta: {
-                title: 'Kitchen',
-            },
-            name: 'kitchen',
-            // route level code-splitting
-            // this generates a separate chunk (read.[hash].js) for this route
-            // which is lazy-loaded when the route is visited.
-            component: () => import(/* webpackChunkName: "read" */ './views/Kitchen.vue')
-        }
-    ]
+  routes: [
+    {
+      path: '/',
+      meta: {
+          title: 'Welcome to Burger Heaven',
+      },
+      name: 'Start',
+      component: StartPage
+    },
+    {
+      path: '/OrderPage',
+      meta: {
+          title: 'Order Screen',
+      },
+      name: 'OrderPage',
+      component: OrderPage
+    },
+    {
+      path: '/OrderSummary',
+      meta: {
+          title: 'Order Summary'
+      },
+      name: 'Summary',
+      component: OrderSummaryPage
+    },
+    {
+      path: '/OrderCompleted',
+      meta: {
+          title: 'Order Completed',
+      },
+      name: 'Completed',
+      component: OrderCompletedPage
+    },
+    {
+      path: '/kitchen',
+      meta: {
+          title: 'Kitchen',
+      },
+      name: 'kitchen',
+      // route level code-splitting
+      // this generates a separate chunk (read.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "read" */ './views/Kitchen.vue')
+    }
+  ]
 });
 
 router.beforeEach((to, from, next) => {
-    document.title = to.meta.title
-    next()
+  document.title = to.meta.title
+  next()
 });
 
 export default router;
--- src/views/StartPage.vue
+++ src/views/StartPage.vue
@@ -1,114 +1,121 @@
 <template>
-<div class="wrapper">
+  <div class="wrapper">
     <div class="container">
-        <div class="flags">
-            <img src="@/assets/sweden.png">
-            <img src="@/assets/uk.png">
+
+      <div class="flags">
+        <img src="@/assets/sweden.png">
+        <img src="@/assets/uk.png">
+      </div>
+
+      <div class="title">
+        EATING LOCATION
+      </div>
+
+      <div class="buttons">
+        <a class="dine_in" href="./#/OrderPage">
+          <img class="dineIN" src="@/assets/dine_in.png">
+        </a>
+        <a class="take_out" href="./#/OrderPage">
+          <img class="takeOUT" src="@/assets/take_out.png">
+        </a>
+      </div>
+      
     </div>
-        <div class="title">
-            EATING LOCATION
-    </div>
-        <div class="buttons">
-            <a class="dine_in" href="./#/OrderPage">
-                <img class="dineIN" src="@/assets/dine_in.png">
-    </a>
-            <a class="take_out" href="./#/OrderPage">
-                <img class="takeOUT" src="@/assets/take_out.png">
-    </a>
-    </div>
-    </div>
-</div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: 'StartPage'
-    }
+export default {
+  name: 'StartPage'
+}
 </script>
 
 <style scoped>
-    @import url('https://fonts.googleapis.com/css?family=Roboto');
-    .wrapper {
-        display: flex;
-        flex-direction: column;
-        margin-bottom: 0px;
-    }
-    .container {
-        display: flex;
-        flex-direction: column;
-        background-color: #2d4739;
-        margin: 0;
-        padding: 0;
-    }
+@import url('https://fonts.googleapis.com/css?family=Roboto');
 
-    a {
-        text-decoration: none;
-    }
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 0px;
+}
 
-    .flags {
-        display: flex;
-        margin-top: 10px;
-        margin-right: 10px;
-        align-self: flex-end;
-    }
+.container {
+  display: flex;
+  flex-direction: column;
+  background-color: #2d4739;
+  margin: 0;
+  padding: 0;
+}
 
-    .title {
-        margin-top: 20px;
-        align-self: center;
-        font-family: Roboto, sans-serif;
-        font-weight: bold;
-        font-size: 60px;
-        text-align: center;
-        color: #fff;
-        text-shadow: 0px 1px 10px rgba(0, 0, 0, 0.7);
-    }
+a {
+  text-decoration: none;
+}
 
-    .buttons {
-        display: flex;
-        flex-direction: row;
-        margin-top: 40px;
-        align-self: center;
-        align-items: center;
-        justify-content: center;
-    }
+.flags {
+  display: flex;
+  margin-top: 10px;
+  margin-right: 10px;
+  align-self: flex-end;
+}
 
-    /*    Responsiveness*/
-    @media screen and (max-width: 760px), screen and (max-height: 420px) {
-        .flags {
-            margin-top: 5px;
-            margin-right: 5px;
-        }
-        .flags img {
-            width: 90px;
-            height: 67px;
-        }
+.title {
+  margin-top: 20px;
+  align-self: center;
+  font-family: Roboto, sans-serif;
+  font-weight: bold;
+  font-size: 60px;
+  text-align: center;
+  color: #fff;
+  text-shadow: 0px 1px 10px rgba(0, 0, 0, 0.7);
+}
 
-        .title {
-            margin-top: 10px;
-            font-size: 50px;
-        }
+.buttons {
+  display: flex;
+  flex-direction: row;
+  margin-top: 40px;
+  align-self: center;
+  align-items: center;
+  justify-content: center;
+}
 
-        .buttons {
-            margin-top: 12px;
-            flex-direction: column;
-        }
+/*    Responsiveness*/
+@media screen and (max-width: 760px), screen and (max-height: 420px) {
+  .flags {
+    margin-top: 5px;
+    margin-right: 5px;
+  }
 
-        .buttons img {
-            width: 154px;
-            height: 168px;
-        }
-    }
-    @media screen and (orientation: landscape) {
-        .buttons {
-            margin-top: 12px;
-            flex-direction: row;
-        }
-    }
+  .flags img {
+    width: 90px;
+    height: 67px;
+  }
 
-    /*iPhone 5/5S/SE*/
-    @media screen and (max-width: 320px) and (max-height: 568px), (max-width: 568px) and (max-height: 320px){
-        .title {
-            font-size: 40px;
-        }
-    }
+  .title {
+    margin-top: 10px;
+    font-size: 50px;
+  }
+
+  .buttons {
+    margin-top: 12px;
+    flex-direction: column;
+  }
+
+  .buttons img {
+    width: 154px;
+    height: 168px;
+  }
+}
+@media screen and (orientation: landscape) {
+  .buttons {
+    margin-top: 12px;
+    flex-direction: row;
+  }
+}
+
+/*iPhone 5/5S/SE*/
+@media screen and (max-width: 320px) and (max-height: 568px), (max-width: 568px) and (max-height: 320px){
+  .title {
+    font-size: 40px;
+  }
+}
 </style>
