562d1149589a5c58254943649a8aa1f3d59a2065
johnlk <jlkuhn@purdue.edu>
2018-12-16 23:22:54 +0100

    cleaned up menu.js's formatting

--- src/components/menu.js
+++ src/components/menu.js
@@ -2,86 +2,70 @@
 
 var menu = {
   burgers: {
-    beef: [
-      {
-        title: 'Double Beef',
-        imgSrc: require('@/assets/Burgers/Beef/beef1.png')
-      },
-      {
-        title: 'Bacon Deli',
-        imgSrc: require('@/assets/Burgers/Beef/beef2.png')
-      },
-      {
-        title: 'Chili Burger',
-        imgSrc: require('@/assets/Burgers/Beef/beef3.png')
-      },
-      {
-        title: 'Big Beef',
-        imgSrc: require('@/assets/Burgers/Beef/beef4.png')
-      },
-      {
-        title: 'Double Meat',
-        imgSrc: require('@/assets/Burgers/Beef/beef5.png')
-      }
-    ],
-    chicken: [
-      {
-        title: 'Chicken Basic',
-        imgSrc: require('@/assets/Burgers/Chicken/chicken1.png')
-      },
-      {
-        title: 'Chicken Deli',
-        imgSrc: require('@/assets/Burgers/Chicken/chicken2.png')
-      },
-      {
-        title: 'Crispy Chicken',
-        imgSrc: require('@/assets/Burgers/Chicken/chicken3.png')
-      },
-      {
-        title: 'Large Chicken',
-        imgSrc: require('@/assets/Burgers/Chicken/chicken4.png')
-      },
-      {
-        title: 'Chicken BBQ',
-        imgSrc: require('@/assets/Burgers/Chicken/chicken5.png')
-      }
-    ],
-    fish: [
-      {
-        title: 'Fish Burger',
-        imgSrc: require('@/assets/Burgers/Fish/fish1.png')
-      },
-      {
-        title: 'Integral Fish',
-        imgSrc: require('@/assets/Burgers/Fish/fish2.png')
-      },
-      {
-        title: 'Long Fish',
-        imgSrc: require('@/assets/Burgers/Fish/fish3.png')
-      }
-    ],
-    vegetarian: [
-      {
-        title: 'Classic Veggie',
-        imgSrc: require('@/assets/Burgers/Veggie/veggie1.png')
-      },
-      {
-        title: 'Pink Veggie',
-        imgSrc: require('@/assets/Burgers/Veggie/veggie2.png')
-      },
-      {
-        title: 'Cripsy Veggie',
-        imgSrc: require('@/assets/Burgers/Veggie/veggie3.png')
-      },
-      {
-        title: 'Typical Veg',
-        imgSrc: require('@/assets/Burgers/Veggie/veggie4.png')
-      },
-      {
-        title: 'Veggie Cool',
-        imgSrc: require('@/assets/Burgers/Veggie/veggie5.png')
-      }
-    ]
+    beef: [{
+      title: 'Double Beef',
+      imgSrc: require('@/assets/Burgers/Beef/beef1.png')
+    }, {
+      title: 'Bacon Deli',
+      imgSrc: require('@/assets/Burgers/Beef/beef2.png')
+    }, {
+      title: 'Chili Burger',
+      imgSrc: require('@/assets/Burgers/Beef/beef3.png')
+    }, {
+      title: 'Big Beef',
+      imgSrc: require('@/assets/Burgers/Beef/beef4.png')
+    }, {
+      title: 'Double Meat',
+      imgSrc: require('@/assets/Burgers/Beef/beef5.png')
+    }],
+    chicken: [{
+      title: 'Chicken Basic',
+      imgSrc: require('@/assets/Burgers/Chicken/chicken1.png')
+    }, {
+      title: 'Chicken Deli',
+      imgSrc: require('@/assets/Burgers/Chicken/chicken2.png')
+    }, {
+      title: 'Crispy Chicken',
+      imgSrc: require('@/assets/Burgers/Chicken/chicken3.png')
+    }, {
+      title: 'Large Chicken',
+      imgSrc: require('@/assets/Burgers/Chicken/chicken4.png')
+    }, {
+      title: 'Chicken BBQ',
+      imgSrc: require('@/assets/Burgers/Chicken/chicken5.png')
+    }],
+    fish: [{
+      title: 'Fish Burger',
+      imgSrc: require('@/assets/Burgers/Fish/fish1.png')
+    }, {
+      title: 'Integral Fish',
+      imgSrc: require('@/assets/Burgers/Fish/fish2.png')
+    }, {
+      title: 'Long Fish',
+      imgSrc: require('@/assets/Burgers/Fish/fish3.png')
+    }],
+    vegetarian: [{
+      title: 'Classic Veggie',
+      imgSrc: require('@/assets/Burgers/Veggie/veggie1.png')
+    }, {
+      title: 'Pink Veggie',
+      imgSrc: require('@/assets/Burgers/Veggie/veggie2.png')
+    }, {
+      title: 'Cripsy Veggie',
+      imgSrc: require('@/assets/Burgers/Veggie/veggie3.png')
+    }, {
+      title: 'Typical Veg',
+      imgSrc: require('@/assets/Burgers/Veggie/veggie4.png')
+    }, {
+      title: 'Veggie Cool',
+      imgSrc: require('@/assets/Burgers/Veggie/veggie5.png')
+    }]
+  },
+  drinks: {
+
+  },
+  sides: {
+    
   }
 };
 
