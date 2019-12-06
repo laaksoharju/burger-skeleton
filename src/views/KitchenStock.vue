205de2fd1aeb0d8841efd345a091b6bcf5373270
jocke <joacim.g.larsson@gmail.com>
2019-12-06 16:48:18 +0100

    fixat mer med inputboxarna

--- src/views/KitchenStock.vue
+++ src/views/KitchenStock.vue
@@ -1,18 +1,17 @@
 <template>
-
 <div id="stock">
-  <img class="example-panel" src= "@/assets/kitchen2.jpeg">
+  <img class="example-panel" src="@/assets/kitchen2.jpeg">
   {{$store.state.hello}}
 
-<h1>{{uiLabels.usedingredients}}</h1>
+  <h1>{{uiLabels.usedingredients}}</h1>
 
-<div id="ingredientsused" v-for="item in ingredients" v-if="item.stock<30" :key="item.ingredient_id">
-  {{item["ingredient_"+ lang]}}: {{item.stock}}
-  <br>
-  add <input v-model.number="num" type="number" id="antalsaker" name="antalsaker"> {{item["ingredient_"+ lang]}} to stock
-  <button id="laggtill" v-on:click="addToStock(item, num)">add</button>
+  <div id="ingredientsused" v-for="item in ingredients" v-if="item.stock<30" :key="item.ingredient_id">
+    {{item["ingredient_"+ lang]}}: {{item.stock}}
+    <br>
+    add <input v-model.number="num[item.ingredient_id]" type="number" id="antalsaker" name="antalsaker"> {{item["ingredient_"+ lang]}} to stock
+    <button id="laggtill" v-on:click="addToStock(item);clearField(item.ingredient_id)">add</button>
 
-</div>
+  </div>
 </div>
 </template>
 
@@ -21,6 +20,7 @@
 import OrderItem from '@/components/OrderItem.vue'
 import OrderItemToPrepare from '@/components/OrderItemToPrepare.vue'
 import Ingredient from '@/components/Ingredient.vue'
+
 
 //import methods and data that are shared between ordering and kitchen views
 import sharedVueStuff from '@/components/sharedVueStuff.js'
@@ -34,41 +34,50 @@ export default {
 
 
   mixins: [sharedVueStuff], // include stuff that is used in both
-                            //the ordering system and the kitchen
-  data: function(){
+  //the ordering system and the kitchen
+  data: function() {
     return {
       chosenIngredients: [],
-      price: 0
+      price: 0,
+      num: {}
     }
   },
-  computed: {
+  computed: {},
+
+  methods: {
+
+    addToStock: function(item) {
+      this.$store.state.socket.emit('updateStock', {
+        ingredient: item
+      }, item.stock + this.num[item.ingredient_id])
     },
 
-  methods: { addToStock: function(item, num){
-    this.$store.state.socket.emit('updateStock', {ingredient: item}, item.stock + num)
+    clearField: function(id) {
+      this.num[id] = null;
+    }
+
   }
-
-
-}
 }
 </script>
 <style scoped>
-.example-panel{
-  position:fixed;
-  z-index:-2;
-  left:0;
-  top:0;
+.example-panel {
+  position: fixed;
+  z-index: -2;
+  left: 0;
+  top: 0;
   opacity: 0.2;
 }
+
 @import 'https://fonts.googleapis.com/css?family=Questrial&display=swap';
 
-#stock{
+#stock {
   font-size: 24pt;
   font-family: 'Questrial', sans-serif;
 
 
 }
-#ingredientsused{
+
+#ingredientsused {
   border: outset firebrick;
   margin: 0.5em;
   display: table;
